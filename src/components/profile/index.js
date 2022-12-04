db66a15f4c9e695054ca15c551344090557d560a
Saket Kumar <kumar.sak@northeastern.edu>
2022-12-03 20:06:48 -0500

    Added support for mydislikes

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, React } from "react";
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import * as service from "../../services/auth-service"
+import MyDislikedTuits from "./my-disliked-tuits";
+import MyLikedTuits from "./my-liked-tuits";
 import MyTuits from "./my-tuits";
 
 const Profile = () => {
@@ -23,29 +25,32 @@ const Profile = () => {
       <h4>{profile.username}</h4>
       <h6>@{profile.username}</h6>
       <button onClick={logout}>Logout</button>
+      <div>
+        <ul className="mt-4 nav nav-pills nav-fill">
+          <li className="nav-item me-3">
+            <Link to="/profile/mytuits"
+              className="nav-link">
+              My Tuits</Link>
+          </li>
+          <li className="nav-item me-3">
+            <Link to="/profile/my-likes"
+              className="nav-link">
+              My Liked Tuits</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/profile/my-dislikes"
+              className="nav-link">
+              My Disliked Tuits</Link>
+          </li>
+        </ul>
+      </div>
       <Routes>
-        <Route index
-          element={(
-            <ul className="mt-4 nav nav-pills nav-fill">
-              <li className="nav-item me-3">
-                <Link to="/profile/mytuits"
-                  className="nav-link active">
-                  My Tuits</Link>
-              </li>
-              <li className="nav-item me-3">
-                <Link to="/profile/my-liked-tuits"
-                  className="nav-link active">
-                  My Liked Tuits</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/profile/my-desliked-tuits"
-                  className="nav-link active">
-                  My Disliked Tuits</Link>
-              </li>
-            </ul>
-          )} />
         <Route path="/mytuits"
           element={<MyTuits uid={profile._id} />} />
+        <Route path="/my-likes"
+          element={<MyLikedTuits uid={profile._id} />} />
+        <Route path="/my-dislikes"
+          element={<MyDislikedTuits uid={profile._id} />} />
       </Routes>
     </div>
   );
